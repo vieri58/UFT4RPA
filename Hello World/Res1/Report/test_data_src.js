bfa3a4c5e9f781ae208161fe469b6262561655d8
vieri58 <vieri.sacchi@microfocus.com>
2021-02-22 17:43:49 +0000

    First Commit

---
+++ Hello World/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\Administrator\\Documents\\Unified Functional Testing\\UFT4RPA\\Hello World\\Res1',namePairs:[]}]};
